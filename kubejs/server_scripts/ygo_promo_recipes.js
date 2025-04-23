564f2e9f7a616ab842ad872446854f9ab8222ba7
Waseef Nayeem <haxerus.corporation@gmail.com>
2025-04-23 00:53:42 -0400

    Added some promos

---
+++ kubejs/server_scripts/ygo_promo_recipes.js
@@ -0,0 +1,41 @@
+ServerEvents.recipes(event => {
+    let card_loot = (id) => {
+        return LootEntry.of("ydm:card", {
+            rarity: "Ultra Rare",
+            code: `${id}_0`,
+            id: parseInt(id),
+            image_index: 0
+        })
+    }
+
+    let card_item = (id) => {
+        return Item.of("ydm:card", {
+            rarity: "Ultra Rare",
+            code: `${id}_0`,
+            id: parseInt(id),
+            image_index: 0
+        })
+    }
+
+    // --- Promo Stuff ---
+
+    // Joey's World Mega Pack
+    event.recipes.create.crushing(['kubejs:n_dust', global.Helpers.get_pack("LCJW").withChance(0.5)], 'kubejs:dm_blank')
+
+    // Seal of Orichalcos
+    event.recipes.create.haunting(['supplementaries:ash', card_item(48179391).withChance(0.01)], 'ydm:card')
+})
+
+
+LootJS.modifiers(event => {
+    // Void Ogre Dragon
+    event
+        .addLootTableModifier('minecraft:entities/ender_dragon')
+        .randomChance(0.5)
+        .addLoot(card_loot(81020646))
+
+    // Volcasaurus
+    event
+        .addLootTableModifier('friendsandfoes:entities/wildfire')
+        .addLoot(card_loot(29669359))
+})
